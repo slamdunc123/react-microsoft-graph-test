7b0934842e344af50d79ffa926da26d97e3352d2
duncan.laidlaw <duncan.laidlaw@deeperthanblue.co.uk>
2019-11-14 10:23:11 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h2>Welcome to MSAL.js Quickstart</h2><br/>
+   
     </div>
   );
 }
